5276eec338cd6cd1fd27d950e4567ad83d2af956
Ryan Mitchell <rtmitchell@google.com>
2019-10-10 10:25:27 -0700

    lavender: Use component-override to configure Bluetooth
    Runtime Resource Overlays (RROs) can no longer change the value of
    resources in manifest when read during PackagerParser.
    
    This change uses component-override to enable the service by default
    on wahoo devices.
    
    Remove usages of profile_supported_sap from overlays as they no
    longer configure the enable state of .sap.SapService.
    
    Change-Id: I885a90c96e27dc36cddab7da41a3fe759de789da

--- device.mk
+++ device.mk
@@ -138,6 +138,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
+# Bluetooth 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
