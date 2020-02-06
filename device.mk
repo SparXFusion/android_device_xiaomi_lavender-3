99e010848e9db4b5c8148f91a7c06e555e8fbf44
Jiyong Park <jiyong@google.com>
2020-02-06 19:02:28 +0900

    lavender: Move VNDK APEX to vendor partition
    ... for aosp_* targets so that we don't need to update vendor.img everytime
    there is a change in the VNDK libraries
    
    Bug: 140136207
    Test: m
    Change-Id: Ic7ffaa9503902884f219400cf93516a6fbf8b210
    (cherry picked from commit 1d97f7055550d375aa7f371e148c3ec9aff0bd29)

--- device.mk
+++ device.mk
@@ -430,7 +430,19 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndk_package \
     libgui_vendor \
-    libstdc++.vendor
+    libstdc++.vendor \
+    vndk-ext \
+    com.android.vndk.current.on_vendor
+	
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-full.so \
+    prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so
+	
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libc++.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libc++.so
 
 # Binder
 PRODUCT_PACKAGES += \
